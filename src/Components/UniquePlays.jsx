3f71439dc8278cfc6ab300f27b6d7bc0af0a74ac
Kanika Rajpal <kanikarajpal22@gmail.com>
2023-05-04 00:32:01 +0530

    final changes

--- src/Components/UniquePlays.jsx
+++ src/Components/UniquePlays.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { VictoryChart, VictoryArea } from 'victory';
 import { scaleDiscontinuous, discontinuitySkipWeekends } from '@d3fc/d3fc-discontinuous-scale';
 
